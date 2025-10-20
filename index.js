1d1fbf80973f3b8099e4b0161dde6eca31255e8c
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-10-20 12:31:53 +0700

    menjalankan server

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ app.request(expess.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.send('Hello World!');
     });
+
+    
+app.listen(port, () => {
+    console.log(`Example app listening on port ${PORT}`);
+    });
