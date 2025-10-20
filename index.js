64891d6f950ccb3dab5fa3312fcf0d181ff656cb
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-10-20 13:32:02 +0700

    PUT

--- index.js
+++ index.js
@@ -70,3 +70,22 @@ app.post('/api/users', (req, res) => {
         }
     );
 });
+
+
+// UPDATE (PUT)
+app.put('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+    const { Name, Nim, Kelas } = req.body;
+
+    db.query(
+        'UPDATE Mahasiswa SET nama = ?, nim = ?, kelas = ? WHERE id = ?',
+        [Name, Nim, Kelas, userId],
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: 'Database error' });
+            }
+            res.json({ message: 'Data berhasil diperbarui' });
+        }
+    );
+});
